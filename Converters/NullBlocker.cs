77b7a0b761b7ca1a131f0e286ea50a463304a012
Gabriel Alves <bielvipbh@hotmail.com>
2025-05-15 22:51:33 -0300

    fix: null safe on numbers

---
+++ Converters/NullBlocker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+
+namespace DravusSensorPanel.Converters;
+
+public class NullBlocker : IValueConverter {
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value;
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value ?? BindingOperations.DoNothing;
+}
